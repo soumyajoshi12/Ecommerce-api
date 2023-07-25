cd607b63968925c6f1546eeb95f0d417f5f0ce51
soumyajoshi12 <113835932+soumyajoshi12@users.noreply.github.com>
2023-07-25 11:08:28 -0700

    Update database.js

--- Backend/models/database.js
+++ Backend/models/database.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 exports.databaseconnection = () => {
   try {
-    mongoose.connect(process.env.MONGODB_URL);
+    mongoose.connect('mongodb+srv://mohit123:mohit123@mohit.tjed4.mongodb.net/api?retryWrites=true&w=majority);
     console.log("Connected Database");
   } catch (err) {
     console.log("Database ERROE >...", err.message);
