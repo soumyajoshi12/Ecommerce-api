7842cc82ab7d33ae276683efda733baf3e169aa3
soumyajoshi12 <113835932+soumyajoshi12@users.noreply.github.com>
2023-07-25 11:50:29 -0700

    Update server.js

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,13 @@ const app = require("./app");
 const dotenv = require("dotenv");
 const { databaseconnection } = require("./models/database");
 
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  next();
+});
+
 // Handling Uncaught Exception
 process.on("uncaughtException", (err) => {
   console.log(`Error: ${err.message}`);
